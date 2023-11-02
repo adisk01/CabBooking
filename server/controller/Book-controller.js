87d0f37ba0e134d89e18efa969c6d59965e06f9a
adisk01 <adiscrazy01@gmail.com>
2023-11-03 01:11:35 +0530

    Mail Done

--- server/controller/Book-controller.js
+++ server/controller/Book-controller.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Book = require("../models/Booking");
+const nodemailer = require("nodemailer")
+// require("dotenv").config
+
 
 const getAllBookings = (req, res) => {
     Book.find()
@@ -33,17 +36,57 @@ const getAllBookings = (req, res) => {
 //         res.status(500).json({error:'failed'});
 //     }
 //     // res.satus(201).json({});
-    
+
 // };
 
-const postCreateBookings = (req, res) => {
-    Book.create(req.body)
-        .then((data) => res.json({ message: "Bookings added successfully", data }))
-        .catch((err) =>
-            res
-                .status(400)
-                .json({ message: "Failed to add todo", error: err.message })
-        );
+const postCreateBookings = async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.ethereal.gmail",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "adityaidfornodemailer@gmail.com", // Your Gmail email address
+            pass: "djrh ggqi ilws kjpd",       // Your Gmail password or an "App Password"
+        },
+    });
+
+    // Define your email content
+    async function sendConfirmationEmail(obj) {
+        console.log(obj.mail);
+        try {
+            const info = await transporter.sendMail({
+                from: 'adityaidfornodemailer@gmail.com', // Sender's email address
+                to: obj.mail,               // Recipient's email address collected from the booking form
+                subject: "Your Booking is confirmed",
+                html: `
+                <h1>Hello,</h1>
+                <p>Your cab booking has been confirmed!</p>
+                <p>Details of your booking:</p>
+                <ul>
+                    <li>Date-Time : ${obj.start_time}</li>
+                    <li>Pickup Location: ${obj.source}</li>
+                    <li>Drop-off Location: ${obj.destination}</li>
+                </ul>
+                <p>Thank you for choosing our cab service. Have a great trip!</p>
+            `,
+            });
+
+            console.log(info.messageId); // Random ID generated after a successful send (optional)
+        } catch (error) {
+            console.error("Error sending confirmation email:", error);
+        }
+    }
+    try {
+        const data = await Book.create(req.body); // Use 'await' to wait for the promise to resolve
+        await sendConfirmationEmail(req.body);
+        res.json({ message: "Bookings added successfully", data });
+    } catch (err) {
+        res
+            .status(400)
+            .json({ message: "Failed to add todo", error: err.message });
+    }
+
 };
 
 
@@ -58,4 +101,4 @@ const postCreateBookings = (req, res) => {
 // };
 
 
-module.exports = { getAllBookings: getAllBookings, postCreateBookings : postCreateBookings} ;
+module.exports = { getAllBookings: getAllBookings, postCreateBookings: postCreateBookings };
